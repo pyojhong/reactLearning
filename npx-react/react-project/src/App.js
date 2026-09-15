bdddec9537c9b2100073e018f2bb06b9d4e5fb14
pyojhong <zerribin@daum.net>
2026-09-15 21:49:01 +0900

    React 기본 문법 공부 완료

---
+++ npx-react/react-project/src/App.js
@@ -0,0 +1,33 @@
+import {useState, useEffect} from 'react';
+
+function App() {
+  const [toDo, setToDo] = useState('');
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => {
+    setToDo(event.target.value);
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === '') {
+      return;
+    }
+    setToDo('');
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    // ... 의 경우 array 의 값을 풀어서 쓰라는 의미이다.
+    // 예시) const food = ['pizza', 'potato']
+    // 예시) const newFood = ['tomato', food] => 이 경우 ['tomato', ['pizza', 'potato']] 가 된다.
+    // 예시) 하지만 const newFood2 = ['tomato', ...food] => 이 경우 ['tomato', 'pizza', 'potato'] 가 된다.
+  };
+
+  return (
+    <div>
+      <h1>투두 리스트 ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input value={toDo} type="text" placeholder='할 일 입력' onChange={onChange}></input>
+        <button>추가</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
