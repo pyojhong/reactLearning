94caa1ed0eb1f9c4261f48d9b8ec134025b39ac5
pyojhong <zerribin@daum.net>
2026-09-17 23:09:56 +0900

    public url 추가

--- npx-react/react-project/src/Movie.js
+++ npx-react/react-project/src/Movie.js
@@ -3,7 +3,7 @@ import Home from './routes/Home';
 import Detail from './routes/Detail';
 
 function Movie() {
-    return <Router>
+    return <Router basename={process.env.PUBLIC_URL}>
         <Routes>
             <Route path='/movie/:id' element={<Detail/>}/>
             <Route path='/' element={<Home />}>
